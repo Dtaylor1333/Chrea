e52f719557a606096a1502afe327578ed608ff7b
Dtaylor1333 <58613334+Dtaylor1333@users.noreply.github.com>
2023-04-01 00:25:12 -0500

    Add files via upload

---
+++ CreateAccount.tsx
@@ -0,0 +1,244 @@
+import React from 'react';
+import {StyleSheet, Image, View, ImageBackground, Text, SafeAreaView, StatusBar, useColorScheme, Touchable, TouchableOpacity, TextInput} from 'react-native';
+import DropdownComponent from './DropdownTemplate';
+import FlatButton from './FlatButton';
+import * as RootNavigation from '../RootNavigation';
+import BottomTabs from '../BottomTabs';
+import BestMoves from './BestMoves';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { useNavigation } from '@react-navigation/native';
+import { Button } from 'react-native';
+
+
+
+export default function CreateAccount(navigation, route){
+
+    const isDarkMode = useColorScheme() === 'dark';
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [passwordError, setPasswordError] = React.useState('');
+    const [confirmPassword, setConfirmPassword] = React.useState('');
+    const [confirmPasswordError, setConfirmPasswordError] = React.useState('');
+
+    const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+};
+
+const handleLogin = () => {
+    if (password.length < 5) {
+      setPasswordError('Password must be at least 5 characters long');
+    } else {
+      setPasswordError('');
+    }
+
+    if (confirmPassword !== password) {
+      setConfirmPasswordError('Passwords do not match');
+    } else {
+      setConfirmPasswordError('');
+    }
+
+    if (password.length >= 5 && confirmPassword === password) {
+      // Perform login logic here
+      console.log(`username: ${username}, Password: ${password}`);
+    }
+  };
+
+    return(
+
+    <ImageBackground style = {styles.backgroundImg} source = {require('../Images/LoadingPageBackground.png')}> 
+        <StatusBar
+            barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+            backgroundColor={backgroundStyle.backgroundColor}/>
+        <View style={styles.container}>
+            <Text style = {styles.text1}>Make an account</Text>
+            <Text style = {styles.text2}>Create your username and password.</Text>
+        </View>
+        <View style={styles.container2}>
+            <Text style={styles.inputTitle}>Username: (5 characters)</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setUsername}
+                value={username}
+                placeholder="username"
+                maxLength={5}
+            />
+            <Text style={styles.inputTitle}>Password: (5 characters)</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setPassword}
+                value={password}
+                placeholder="password"
+                secureTextEntry={true}
+                maxLength={5}
+            />
+            {passwordError ? <Text>{passwordError}</Text> : null}
+            <Text style={styles.inputTitle}>Confirm Password</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setConfirmPassword}
+                value={confirmPassword}
+                placeholder="password"
+                secureTextEntry={true}
+                maxLength={5}
+            />
+            {confirmPasswordError ? <Text>{confirmPasswordError}</Text> : null} 
+            {passwordError ? <Text>{passwordError}</Text> : null}
+            <Button title="Already have an account" onPress={() => navigation.navigate("Login Page")}/>
+            <View style={styles.container2}>
+                <View>
+                <TouchableOpacity onPress={handleLogin}>
+                    <View style = {styles.button2}>
+                        <Text style = {styles.buttonText3}> Confirm</Text>
+                    </View>
+                </TouchableOpacity>
+                </View>
+                
+                <View>
+                <TouchableOpacity>
+                    <View style = {styles.button3}>
+                        <Text style = {styles.buttonText4}> Cancel</Text>
+                    </View>
+                </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    </ImageBackground>
+    );
+}
+
+
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex:1,
+        // borderColor: 'red',
+        // borderWidth: 4,
+        },
+    
+    container2:{
+        flex: 2,
+        marginTop: 60,
+    },
+
+    input: {
+        height: 40,
+        margin: 12,
+        marginBottom: 0,
+        borderWidth: 1,
+        padding: 10,
+        width: 300,
+      },
+
+    text1:{
+        fontSize: 36,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: '#3c1c07',
+        marginTop: 150,
+        },
+
+    text2:{
+        fontSize: 18,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: '#a38c84',
+        marginTop: 20,
+        width: 300,
+    },
+
+    inputTitle:{
+        fontSize: 18,
+        marginTop: 10,
+    },
+
+    backgroundImg: {
+        // flex:1,
+        height: '102%',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    button: {
+        borderRadius: 13,
+        paddingVertical: 4,
+        paddingHorizontal: 8,
+        backgroundColor: 'transparent',
+        borderColor: 'rgba(163, 140, 132, .5)',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        width: 150,
+        height: 50,
+        marginTop: 100,
+        marginRight: 10,
+    },
+
+    button4: {
+        borderRadius: 13,
+        paddingVertical: 4,
+        paddingHorizontal: 8,
+        backgroundColor: 'transparent',
+        borderColor: 'rgba(163, 140, 132, .5)',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        width: 150,
+        height: 50,
+        marginTop: 100,
+        marginLeft: 10,
+    },
+
+    button2: {
+        borderRadius: 20,
+        paddingVertical: 14,
+        paddingHorizontal: 3,
+        backgroundColor: '#3c1c07',
+        width: 160,
+        height: 50,
+        alignSelf: 'center',
+        
+    },
+
+    button3: {
+        borderRadius: 20,
+        paddingVertical: 14,
+        paddingHorizontal: 3,
+        backgroundColor: 'transparent',
+        borderColor: 'black',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        width: 160,
+        height: 50,
+        alignSelf: 'center',
+        marginTop: 15,
+    },
+
+    buttonText1: {
+        color: '#a38c84',
+        fontWeight: 'bold',
+        fontSize: 35,
+        textAlign: 'center',
+    },
+
+    buttonText2: {
+        color: '#a38c84',
+        fontWeight: 'bold',
+        fontSize: 35,
+        textAlign: 'center',
+    },
+
+    buttonText3: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+        textAlign: 'center',
+    },
+
+    buttonText4: {
+        color: '#3c1c07',
+        fontWeight: 'bold',
+        fontSize: 16,
+        textAlign: 'center',
+    },
+
+});
---
+++ WelcomePage.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import {StyleSheet, Image, View, Text, Button, Pressable, SafeAreaView, StatusBar, useColorScheme, TouchableOpacity} from 'react-native';
+import { Colors, DebugInstructions, Header, LearnMoreLinks, ReloadInstructions,} from 'react-native/Libraries/NewAppScreen';
+import * as RootNavigation from '../RootNavigation';
+import PlaceSelectPage from "./PlaceSelectPage";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+
+
+
+
+// const WelcomePage = () => {
+//     // const { handlePress, title = 'Get Started' } = props;
+//     // const handlePress = () => false;
+
+//     const isDarkMode = useColorScheme() === 'dark';
+
+//     // const navigation = useNavigation();
+
+//     const backgroundStyle = {
+//       backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//     };
+
+//     return(
+        
+//         // <SafeAreaView>
+            
+//             <View>
+//                 <StatusBar
+//             barStyle={isDarkMode ? 'light-content' : 'light-content'}
+//             backgroundColor={backgroundStyle.backgroundColor}/>
+//                 <Image style = {styles.image1} source={require('../Images/Welcome.jpg')} />
+//                 <Text style = {styles.text1}> Welcome to Chrea</Text>
+//                 <Text style = {styles.text2}> Decide where to spend your {"\n"}time faster. </Text>
+//                 <Pressable style={styles.button} >
+//                     <Text style={styles.buttonText}></Text>
+//                 </Pressable>
+//                 {/* <Button style ={styles.button} title="Learn More"></Button> */}
+//             </View>
+//         // </SafeAreaView>
+//     );
+// };
+
+
+// export default WelcomePage;
+
+export default function WelcomePage({navigation}: any){
+    
+    const isDarkMode = useColorScheme() === 'dark';
+
+    const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+};
+
+    return (
+        // <SafeAreaView>
+            
+        <View>
+        <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'light-content'}
+        backgroundColor={backgroundStyle.backgroundColor}/>
+        <Image style = {styles.image1} source={require('../Images/Welcome.jpg')} />
+        <Text style = {styles.text1}> Welcome to Chrea</Text>
+        <Text style = {styles.text2}> Decide where to spend your {"\n"}time faster. </Text>
+        <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate("Create an Account")}>
+            <Text style={styles.buttonText}>Get Started</Text>
+        </TouchableOpacity>
+        {/* /* <Button style ={styles.button} title="Learn More"></Button> */}
+    </View>
+// </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+
+    image1:{
+        height: 550,
+        maxWidth: 400,
+        justifyContent: 'center',
+        alignItems: 'center',
+        },
+
+    text1:{
+        fontSize: 38,
+        textAlign: 'center',
+        marginTop: 25,
+        fontWeight: 'bold',
+        color: '#3c1c07',
+        },
+
+    text2:{
+        fontSize: 18,
+        textAlign: 'center',
+        marginTop: 15,
+        color: '#a38c84',
+        },
+
+    buttonText:{
+        color: 'white',
+        fontSize: 18,
+    },
+
+    button:{
+        color: '#c65304',
+        borderRadius: 18,
+        marginTop: 55,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#C65304',
+        marginLeft: 53,
+        marginRight: 50,
+        marginBottom: 60,
+        padding: 15,
+    },
+})
